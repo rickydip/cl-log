95fa944d15271569b3558e9c60f0f913248632aa
Riccardo Di Pietro <dipcisco@hotmail.com>
2014-03-10 18:39:04 +0100

    18.38 10.03.2014 modifiche finali

--- trunk/clevercloud/sources/org/clever/Common/LoggingPlugins/Log4J/Log4J.java
+++ trunk/clevercloud/sources/org/clever/Common/LoggingPlugins/Log4J/Log4J.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2014 riccardo.
+ * Copyright 2014 Riccardo Di Pietro.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
